89055d0080b7d0b90fad605da3b3ea81b5ed87cd
alex <alexanderlieepm@gmail.com>
2024-05-05 19:52:26 +0700

    update

--- BACKEND/api/database/migrations/2014_10_12_000000_create_users_table.php
+++ BACKEND/api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,23 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('employee_id')->nullable();
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+            $table->timestamp('two_factor_confirmed_at')->nullable();
             $table->rememberToken();
+            $table->bigInteger('current_team_id')->unsigned()->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('status');
+            $table->string('created_by', 10)->nullable();
             $table->timestamps();
+            $table->string('updated_by', 10)->nullable();
+            $table->softDeletes('deleted_at');
+            $table->string('deleted_by', 10)->nullable();
         });
     }
 
