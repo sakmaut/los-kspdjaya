ecdb1cf743cdaa49697f3fc085522df8729b6791
ahapsin <ahapsin02@gmail.com>
2024-06-02 02:45:34 +0700

    update

--- FRONTEND/los_app/src/router/index.js
+++ FRONTEND/los_app/src/router/index.js
@@ -1,113 +1,95 @@
 import TestComp from "@/components/TestComponent.vue";
-import ApplyCredit from "@/components/organism/ApplyCredit.vue";
-import PageNotFound from "@/views/404PageNotFound.vue";
 import AccountSettings from "@/views/AccountSettings.vue";
-import ApplyPage from "@/views/ApplyPage.vue";
-import VisitPage from "@/views/VisitPage.vue";
-import VisitAdminPage from "@/views/VisitAdminPage.vue";
-import ApprovalPage from "@/views/ApprovalPage.vue";
-import DetailSurveyPage from "@/views/DetailSurveyPage.vue";
-import BerandaView from "@/views/BerandaView.vue";
-import Dashboard from "@/views/DashBoard.vue";
-import DetailProspectPage from "@/views/DetailProspectPage.vue";
 import GeneralSettings from "@/views/GeneralSettings.vue";
 import HomePage from "@/views/HomePage.vue";
-import LandingPage from "@/views/LandingPage.vue";
-import LoginPage from "@/views/LoginPage.vue";
-import ConfPage from "@/views/SPA/ConfPage.vue";
-import MutasiRekening from "@/views/SPA/MutasiRekening.vue";
-import RekeningKoranSPA from "@/views/SPA/RekeningKoranSPA.vue";
-import TestView from "@/views/TestView.vue";
-import AddMenuView from "@/views/master/addMenuView.vue";
-import BranchView from "@/views/master/branchView.vue";
 import EmployeesView from "@/views/master/employeesView.vue";
 import MasterView from "@/views/master/masterView.vue";
-import MenuView from "@/views/master/menuView.vue";
 import SettingsView from "@/views/master/settingsView.vue";
 import UsersView from "@/views/master/usersView.vue";
-import MainVisitDisplay from "@/views/task/visit/MainVisitDisplay.vue";
 import { createRouter, createWebHistory } from "vue-router";
 // slice
-import BerandaSlice from "@/components/slice/BerandaSlice.vue";
 
 const routes = [
-  { path: "/", name: "landing", component: LandingPage },
-  { path: "/beranda-slice", name: "beranda slice", component: BerandaSlice },
-  { path: "/beranda", name: "beranda", component: BerandaView },
-  { path: "/login", name: "Login", component: LoginPage },
   {
-    path: "/dashboard",
-    name: "Home",
-    component: Dashboard,
-    meta: { requiresAuth: true },
-    children: [
-      {
-        path: "emails",
-        component: TestView,
-      },
-    ],
+    path: "/",
+    name: "landing",
+    component: () => import("@/views/LandingPage.vue"),
   },
-  { path: "/apply-loan", component: ApplyPage },
+  {
+    path: "/beranda-slice",
+    name: "beranda slice",
+    component: () => import("@/components/slice/BerandaSlice.vue"),
+  },
+  {
+    path: "/beranda",
+    name: "beranda",
+    component: () => import("@/views/BerandaView.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/LoginPage.vue"),
+  },
   {
     path: "/task",
     name: "task",
     props: true,
-    component: BerandaView,
+    component: () => import("@/views/BerandaView.vue"),
     children: [
       {
         path: "new-survey",
         name: "input kunjungan",
-        component: ApplyPage,
+        component: () => import("@/views/ApplyPage.vue"),
       },
       {
         path: "survey",
         name: "Survey",
-        component: VisitPage,
+        component: () => import("@/views/VisitPage.vue"),
       },
       {
         path: "survey-admin",
         name: "Survey Admin",
-        component: VisitAdminPage,
+        component: () => import("@/views/VisitAdminPage.vue"),
       },
       {
         name: "Detail survey",
         path: "survey/:surveyId",
-        component: DetailSurveyPage,
+        component: () => import("@/views/DetailSurveyPage.vue"),
       },
       {
         name: "Detail approval survey",
         path: "approval/:surveyId",
-        component: DetailSurveyPage,
+        component: () => import("@/views/DetailSurveyPage.vue"),
       },
       {
         path: "approval",
         name: "approval",
-        component: ApprovalPage,
+        component: () => import("@/views/ApprovalPage.vue"),
       },
       {
         name: "Form Pengajuan Kredit",
         path: "apply-credit",
-        component: VisitAdminPage,
+        component: () => import("@/views/VisitAdminPage.vue"),
       },
       {
         name: "Form Kredit",
         path: "form-credit/:prospect",
-        component: ApplyCredit,
+        component: import("@/components/organism/ApplyCredit.vue"),
       },
       {
         path: "visit",
-        component: MainVisitDisplay,
+        component: () => import("@/views/task/visit/MainVisitDisplay.vue"),
       },
       {
         path: "visit/:prospect",
-        component: DetailProspectPage,
+        component: () => import("@/views/DetailProspectPage.vue"),
       },
     ],
   },
   {
     path: "/pengaturan",
     name: "akun",
-    component: BerandaView,
+    component: () => import("@/views/BerandaView.vue"),
     meta: { requiresAuth: true },
     children: [
       {
@@ -129,12 +111,12 @@ const routes = [
       {
         name: "menu",
         path: "menu",
-        component: MenuView,
+        component: () => import("@/views/master/menuView.vue"),
       },
       {
         name: "cabang",
         path: "branch",
-        component: BranchView,
+        component: () => import("@/views/master/branchView.vue"),
       },
       {
         name: "tambah cabang",
@@ -169,29 +151,21 @@ const routes = [
       {
         name: "Tambah Menu",
         path: "add-menu",
-        component: AddMenuView,
+        component: () => import("@/views/master/addMenuView.vue"),
       },
     ],
   },
-  { path: "/test-view", name: "Test", component: TestView },
   { path: "/home", name: "home", component: HomePage },
   { path: "/test-comp", name: "TestC", component: TestComp },
   {
     path: "/konfirmasi/:id",
     name: "conf-page",
-    component: ConfPage,
+    component: () => import("@/views/SPA/ConfPage.vue"),
   },
   {
-    path: "/history-pay-spa",
-    name: "history-pay",
-    component: RekeningKoranSPA,
+    path: "/:pathMatch(.*)*",
+    component: () => import("@/views/404PageNotFound.vue"),
   },
-  {
-    path: "/history-acc-spa",
-    name: "history-rekening",
-    component: MutasiRekening,
-  },
-  { path: "/:pathMatch(.*)*", component: PageNotFound },
 ];
 
 const router = createRouter({
--- FRONTEND/los_app/vite.config.js
+++ FRONTEND/los_app/vite.config.js
@@ -1,18 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
